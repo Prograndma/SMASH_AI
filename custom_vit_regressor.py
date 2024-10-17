1837e9199f944b5c94db1a6701e0da86bc954cc7
Prograndma <thomasvcole@gmail.com>
2024-10-17 15:08:21 -0600

    This was previously un-used but intelligently removed from vit.py. I updated it to match and added a few helpers to prepare for using this class instead of the other copycat class defined somewhere else.

--- custom_vit_regressor.py
+++ custom_vit_regressor.py
@@ -1,18 +1,18 @@
 import torch
 import torch.nn as nn
-from transformers import ViTModel
+from transformers import ViTModel, ViTImageProcessor
 import os
 
-
-WHAT_WE_WORKING_ON = "balanced"
-VIT_BASE_DIR = f"vit/{WHAT_WE_WORKING_ON}"
-VIT_BASE_FILENAME = f"{VIT_BASE_DIR}/checkpoints"
 
 HUGGING_FACE_PRETRAINED_VIT = "google/vit-base-patch16-224-in21k"
 
 
 class CustomViTRegressor(nn.Module):
-    def __init__(self, should_load_from_disk=True, base_filename=VIT_BASE_FILENAME, base_dir=VIT_BASE_DIR):
+    """
+        base_dir should take into account which game is being trained on and which dataset is being used
+        base_filename should be where the checkpoints of the model are/should be
+    """
+    def __init__(self, base_dir, base_filename, should_load_from_disk=True):
         super(CustomViTRegressor, self).__init__()
         if should_load_from_disk:
             self.base_model = ViTModel.from_pretrained(f"{base_filename}/pretrained")
@@ -44,3 +44,33 @@ class CustomViTRegressor(nn.Module):
 
     def save(self, epoch):
         torch.save(self.state_dict(), f"{self.base_filename}/{epoch}")
+
+    @staticmethod
+    def custom_data_collator_function():
+        def return_func(batch):
+            images = [item["image"] for item in batch]
+
+            inputs = ViTImageProcessor(images, return_tensors="pt")
+            targets = torch.tensor([[
+                item["Start"],
+                item["A"],
+                item["B"],
+                item["X"],
+                item["Y"],
+                item["Z"],
+                item["DPadUp"],
+                item["DPadDown"],
+                item["DPadLeft"],
+                item["DPadRight"],
+                item["L"],
+                item["R"],
+                item["LPressure"] / 255,
+                item["RPressure"] / 255,
+                item["XAxis"] / 255,
+                item["YAxis"] / 255,
+                item["CXAxis"] / 255,
+                item["CYAxis"] / 255] for item in batch], dtype=torch.float32)
+
+            return inputs, targets
+
+        return return_func
