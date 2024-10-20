627bfc544ec4c7081fb106f2fb680c51ffba4b58
Prograndma <thomasvcole@gmail.com>
2024-10-19 23:43:05 -0600

    Removing the TODO and fixing run_tests function. It's untested code.

--- best_model_checkpoint.py
+++ best_model_checkpoint.py
@@ -20,20 +20,20 @@ class BestModelCheckpoint:
         checkpoints = (file for file in os.listdir(self.model_checkpoints_folder)
                        if os.path.isfile(os.path.join(self.model_checkpoints_folder, file)))
         checkpoints = sorted(checkpoints)
+        validation_losses = []
         for checkpoint in checkpoints:
-            # TODO: There needs to be two different validation_losses lists or something.
-            model = self.model_class(base_dir=None, base_filename=self.model_checkpoints_folder, should_load_from_disk=False)
+            model = self.model_class(base_dir=None, base_filename=self.model_checkpoints_folder,
+                                     should_load_from_disk=False)
             model.update_model_from_checkpoint(checkpoint)
-            validation_losses = []
 
+            temp_val = []
             for batch_index, (x_l, y_l) in enumerate(self.validation_loader):
                 x_v, y_v = x_l['pixel_values'].to(self.device), y_l.to(self.device)
-                validation_losses.append(self.objective(model(x_v), y_v).item())
-            val = np.mean(validation_losses)
-            validation_loss_values.append((checkpoint, val))
-            validation_losses = []
+                temp_val.append(self.objective(model(x_v), y_v).item())
+            val = np.mean(temp_val)
+            validation_losses.append((checkpoint, val))
 
 
-            with open(f"{self.validation_losses_outfile}", "w") as f:
-                for val_loss in test_losses:
-                    f.write(str(val_loss) + "\n")
+        with open(f"{self.validation_losses_outfile}", "w") as f:
+            for val_loss in validation_losses:
+                f.write(str(val_loss) + "\n")
