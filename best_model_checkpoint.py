1188352a99237f24dfa1d5aff12a738980e2c5b8
Prograndma <thomasvcole@gmail.com>
2024-10-20 00:23:17 -0600

    adding a way to run this as main, also fixing the models not being sent to the GPU.

--- best_model_checkpoint.py
+++ best_model_checkpoint.py
@@ -21,19 +21,43 @@ class BestModelCheckpoint:
                        if os.path.isfile(os.path.join(self.model_checkpoints_folder, file)))
         checkpoints = sorted(checkpoints)
         validation_losses = []
-        for checkpoint in checkpoints:
-            model = self.model_class(base_dir=None, base_filename=self.model_checkpoints_folder,
-                                     should_load_from_disk=False)
-            model.update_model_from_checkpoint(checkpoint)
+        print("Starting validation testing on all checkpoints")
+        print(f"batches per checkpoint: {len(self.validation_loader)}")
+        with torch.no_grad():
+            for checkpoint in checkpoints:
+                print(f"Checkpoint {checkpoint} starting")
+                model = self.model_class(base_dir=None, base_filename=self.model_checkpoints_folder)
+                model.update_model_from_checkpoint(checkpoint)
+                model = model.to(self.device)
 
-            temp_val = []
-            for batch_index, (x_l, y_l) in enumerate(self.validation_loader):
-                x_v, y_v = x_l['pixel_values'].to(self.device), y_l.to(self.device)
-                temp_val.append(self.objective(model(x_v), y_v).item())
-            val = np.mean(temp_val)
-            validation_losses.append((checkpoint, val))
+                temp_val = []
+                for batch_index, (x_l, y_l) in enumerate(self.validation_loader):
+                    x_v, y_v = x_l['pixel_values'].to(self.device), y_l.to(self.device)
+                    temp_val.append(self.objective(model(x_v), y_v).item())
+                val = np.mean(temp_val)
+                validation_losses.append((checkpoint, val))
 
 
         with open(f"{self.validation_losses_outfile}", "w") as f:
             for val_loss in validation_losses:
                 f.write(str(val_loss) + "\n")
+
+
+if __name__ == "__main__":
+    from datasets import load_from_disk
+    from custom_vit_regressor import CustomViTRegressor
+    import torch
+    from torch.utils.data import DataLoader
+
+    dataset_path = "/home/thomas/PycharmProjects/SMASH_AI/dataset/smash/balanced"
+    dataset = load_from_disk(dataset_path)
+
+    custom_data_collator = CustomViTRegressor.custom_data_collator_function()
+    val_loader = DataLoader(dataset["validation"], batch_size=32, collate_fn=custom_data_collator)
+    bmc = BestModelCheckpoint(model_class=CustomViTRegressor,
+                              model_checkpoints_folder="/home/thomas/PycharmProjects/SMASH_AI/vit/smash/balanced_no_shuffle_mse_loss/checkpoints",
+                              validation_loader=val_loader,
+                              objective=torch.nn.MSELoss(),
+                              validation_losses_outfile="/home/thomas/PycharmProjects/SMASH_AI/vit/smash/balanced_no_shuffle_mse_loss/val_losses.txt",
+                              )
+    bmc.run_tests()
