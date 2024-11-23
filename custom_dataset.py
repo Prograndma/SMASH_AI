b692ee0721d59c2abab1e37f3a823d8cc9420912
Prograndma <thomasvcole@gmail.com>
2024-11-23 13:16:58 -0700

    Adding an option to make a datset culled.

--- custom_dataset.py
+++ custom_dataset.py
@@ -4,8 +4,9 @@ import torch
 
 
 class CustomDataset(Dataset):
-    def __init__(self, dataset):
+    def __init__(self, dataset, culled):
         self.dataset = dataset
+        self.culled = culled
         self.transform = transforms.Compose([
             transforms.ToTensor()
         ])
@@ -16,26 +17,47 @@ class CustomDataset(Dataset):
     def __getitem__(self, idx):
         sample = self.dataset[idx]
         image = self.transform(sample['image'])
-
-        targets = torch.tensor([
-            sample["Start"],
-            sample["A"],
-            sample["B"],
-            sample["X"],
-            sample["Y"],
-            sample["Z"],
-            sample["DPadUp"],
-            sample["DPadDown"],
-            sample["DPadLeft"],
-            sample["DPadRight"],
-            sample["L"],
-            sample["R"],
-            sample["LPressure"] / 255,
-            sample["RPressure"] / 255,
-            sample["XAxis"] / 255,
-            sample["YAxis"] / 255,
-            sample["CXAxis"] / 255,
-            sample["CYAxis"] / 255],
-            dtype=torch.float32)
+        if self.culled:
+            targets = torch.tensor([
+                # sample["Start"],
+                sample["A"],
+                sample["B"],
+                sample["X"],
+                # sample["Y"],
+                sample["Z"],
+                # sample["DPadUp"],
+                # sample["DPadDown"],
+                # sample["DPadLeft"],
+                # sample["DPadRight"],
+                # sample["L"],
+                # sample["R"],
+                sample["LPressure"] / 255,
+                sample["RPressure"] / 255,
+                sample["XAxis"] / 255,
+                sample["YAxis"] / 255,
+                sample["CXAxis"] / 255,
+                sample["CYAxis"] / 255],
+                dtype=torch.float32)
+        else:
+            targets = torch.tensor([
+                sample["Start"],
+                sample["A"],
+                sample["B"],
+                sample["X"],
+                sample["Y"],
+                sample["Z"],
+                sample["DPadUp"],
+                sample["DPadDown"],
+                sample["DPadLeft"],
+                sample["DPadRight"],
+                sample["L"],
+                sample["R"],
+                sample["LPressure"] / 255,
+                sample["RPressure"] / 255,
+                sample["XAxis"] / 255,
+                sample["YAxis"] / 255,
+                sample["CXAxis"] / 255,
+                sample["CYAxis"] / 255],
+                dtype=torch.float32)
 
         return {'image': image, 'targets': targets}
