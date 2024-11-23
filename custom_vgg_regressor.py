ec01bbfc2f1f5bce5232185f96c534301506c577
Prograndma <thomasvcole@gmail.com>
2024-11-23 13:23:49 -0700

    Removing this from a different file and creating a class file.

---
+++ custom_vgg_regressor.py
@@ -0,0 +1,141 @@
+import torch
+from torch import nn
+import os
+from custom_image_processor import CustomImageProcessor
+
+
+class CustomVGGRegressor(nn.Module):
+    def __init__(self, base_filename):
+        super(CustomVGGRegressor, self).__init__()
+        self.base_filename = base_filename
+        self.layer1 = nn.Sequential(
+            nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(64),
+            nn.ReLU())
+        self.layer2 = nn.Sequential(
+            nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2))
+        self.layer3 = nn.Sequential(
+            nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(128),
+            nn.ReLU())
+        self.layer4 = nn.Sequential(
+            nn.Conv2d(128, 128, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(128),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2))
+        self.layer5 = nn.Sequential(
+            nn.Conv2d(128, 256, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(256),
+            nn.ReLU())
+        self.layer6 = nn.Sequential(
+            nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(256),
+            nn.ReLU())
+        self.layer7 = nn.Sequential(
+            nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(256),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2))
+        self.layer8 = nn.Sequential(
+            nn.Conv2d(256, 512, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(512),
+            nn.ReLU())
+        self.layer9 = nn.Sequential(
+            nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(512),
+            nn.ReLU())
+        self.layer10 = nn.Sequential(
+            nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(512),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2))
+        self.layer11 = nn.Sequential(
+            nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(512),
+            nn.ReLU())
+        self.layer12 = nn.Sequential(
+            nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(512),
+            nn.ReLU())
+        self.layer13 = nn.Sequential(
+            nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(512),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2))
+        self.fc = nn.Sequential(
+            nn.Dropout(0.5),
+            nn.Linear(7*9*512, 4096),
+            nn.ReLU())
+        self.fc1 = nn.Sequential(
+            nn.Dropout(0.5),
+            nn.Linear(4096, 4096),
+            nn.ReLU())
+        self.fc2 = nn.Sequential(
+            nn.Linear(4096, 11))   # culling buttons Y, dp-updownleftright, lpressure and Rpressure
+
+    def forward(self, x):
+        out = self.layer1(x)
+        out = self.layer2(out)
+        out = self.layer3(out)
+        out = self.layer4(out)
+        out = self.layer5(out)
+        out = self.layer6(out)
+        out = self.layer7(out)
+        out = self.layer8(out)
+        out = self.layer9(out)
+        out = self.layer10(out)
+        out = self.layer11(out)
+        out = self.layer12(out)
+        out = self.layer13(out)
+        out = out.reshape(out.size(0), -1)
+        out = self.fc(out)
+        out = self.fc1(out)
+        out = self.fc2(out)
+        return out
+
+    def saved_model_exists(self, checkpoint):
+        if not os.path.exists(f"{self.base_filename}/{checkpoint}"):
+            return False
+        return True
+
+    def update_model_from_checkpoint(self, checkpoint):
+        if not self.saved_model_exists(checkpoint):
+            return "no saved model exists"
+
+        path = f"{self.base_filename}/{checkpoint}"
+
+        loaded = torch.load(path)
+        return self.load_state_dict(loaded)
+
+    def save(self, epoch):
+        if not os.path.isdir(self.base_filename):
+            os.mkdir(self.base_filename)
+        output_filename = f"{self.base_filename}/{epoch}"
+        torch.save(self.state_dict(), output_filename)
+
+    @staticmethod
+    def custom_data_collator_function():
+        processor = CustomImageProcessor()
+        def return_func(batch):
+            images = [item["image"] for item in batch]
+
+            inputs = processor(images, return_tensors="pt")
+            targets = torch.tensor([[
+                item["Start"],
+                item["A"],
+                item["B"],
+                item["X"],
+                item["Z"],
+                item["LPressure"] / 255,
+                item["RPressure"] / 255,
+                item["XAxis"] / 255,
+                item["YAxis"] / 255,
+                item["CXAxis"] / 255,
+                item["CYAxis"] / 255] for item in batch], dtype=torch.float32)
+
+            return inputs, targets
+
+        return return_func
