7d50a8700f86bcc0fbda79601201b58892a07712
Prograndma <thomasvcole@gmail.com>
2024-11-23 13:17:54 -0700

    Added an option to cull outputs.

--- custom_vit_regressor.py
+++ custom_vit_regressor.py
@@ -11,15 +11,19 @@ class CustomViTRegressor(nn.Module):
     """
         base_filename should be where the checkpoints of the model are/should be
     """
-    def __init__(self, base_filename, should_load_from_disk=True):
+    def __init__(self, base_filename, cull, should_load_from_disk=True):
         self.threshold = None
+        self.cull = cull
         super(CustomViTRegressor, self).__init__()
         if should_load_from_disk:
             self.base_model = ViTModel.from_pretrained(f"{base_filename}/pretrained")
         else:
             self.base_model = ViTModel.from_pretrained(HUGGING_FACE_PRETRAINED_VIT)
             self.base_model.save_pretrained(f"{base_filename}/pretrained")
-        self.regressor = nn.Linear(self.base_model.config.hidden_size, 18)  # amount of controller inputs
+        if self.cull:
+            self.regressor = nn.Linear(self.base_model.config.hidden_size, 11)  # amount of culled controller inputs
+        else:
+            self.regressor = nn.Linear(self.base_model.config.hidden_size, 18)  # amount of controller inputs
         self.base_filename = base_filename
 
     def forward(self, x):
@@ -59,31 +63,51 @@ class CustomViTRegressor(nn.Module):
         torch.save(self.state_dict(), f"{self.base_filename}/{epoch}")
 
     @staticmethod
-    def custom_data_collator_function():
-        def return_func(batch):
-            images = [item["image"] for item in batch]
-            temp_processor = ViTImageProcessor()
-            inputs = temp_processor(images, return_tensors="pt")
-            targets = torch.tensor([[
-                item["Start"],
-                item["A"],
-                item["B"],
-                item["X"],
-                item["Y"],
-                item["Z"],
-                item["DPadUp"],
-                item["DPadDown"],
-                item["DPadLeft"],
-                item["DPadRight"],
-                item["L"],
-                item["R"],
-                item["LPressure"] / 255,
-                item["RPressure"] / 255,
-                item["XAxis"] / 255,
-                item["YAxis"] / 255,
-                item["CXAxis"] / 255,
-                item["CYAxis"] / 255] for item in batch], dtype=torch.float32)
+    def custom_data_collator_function(cull):
+        if cull:
+            def return_func(batch):
+                images = [item["image"] for item in batch]
+                temp_processor = ViTImageProcessor()
+                inputs = temp_processor(images, return_tensors="pt")
+                targets = torch.tensor([[
+                    item["Start"],
+                    item["A"],
+                    item["B"],
+                    item["X"],
+                    item["Z"],
+                    item["LPressure"] / 255,
+                    item["RPressure"] / 255,
+                    item["XAxis"] / 255,
+                    item["YAxis"] / 255,
+                    item["CXAxis"] / 255,
+                    item["CYAxis"] / 255] for item in batch], dtype=torch.float32)
 
-            return inputs, targets
+                return inputs, targets
+        else:
+            def return_func(batch):
+                images = [item["image"] for item in batch]
+                temp_processor = ViTImageProcessor()
+                inputs = temp_processor(images, return_tensors="pt")
+                targets = torch.tensor([[
+                    item["Start"],
+                    item["A"],
+                    item["B"],
+                    item["X"],
+                    item["Y"],
+                    item["Z"],
+                    item["DPadUp"],
+                    item["DPadDown"],
+                    item["DPadLeft"],
+                    item["DPadRight"],
+                    item["L"],
+                    item["R"],
+                    item["LPressure"] / 255,
+                    item["RPressure"] / 255,
+                    item["XAxis"] / 255,
+                    item["YAxis"] / 255,
+                    item["CXAxis"] / 255,
+                    item["CYAxis"] / 255] for item in batch], dtype=torch.float32)
+
+                return inputs, targets
 
         return return_func
